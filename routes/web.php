669ba0f8e784354a25d5c9eaee15095d578860f3
gab_alephit <gabriel@alephit.com.au>
2020-07-30 09:58:24 +0800

    fixes command

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::post("/build", function(Request $request) {
     $data = collect($request->post());
     $branch = $data->has("pullrequest") ? "develop" : "master";
-    $this->info("Deploying {$branch} branch");
+    echo "<pre>Deploying {$branch} branch</pre>";
     Artisan::call("build {$branch}");
-    $this->info("Running database migration");
+    echo "<pre>Running database migration</pre>";
     Artisan::call("migrate --seed");
-    $this->info("Deployment complete");
+    echo "<pre>Deployment complete</pre>";
 });
