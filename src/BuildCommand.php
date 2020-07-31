3ff049db9901577c62a5629041c5e285eed805d8
gab_alephit <gabriel@alephit.com.au>
2020-07-31 12:02:31 +0800

    removes --no-dev --no-scripts option when executing composer install

--- src/BuildCommand.php
+++ src/BuildCommand.php
@@ -20,7 +20,7 @@ class BuildCommand extends Command
         $output = shell_exec("git pull origin {$branch}");
         echo "<pre>$output</pre>";
 
-        $output = shell_exec("composer install --no-dev --no-scripts");
+        $output = shell_exec("composer install");
         echo "<pre>$output</pre>";
 
         echo "<pre>Running database migration</pre>";
