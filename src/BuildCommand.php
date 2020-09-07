81860ad079f91e97a9be321a0c983b5dfe8fba0a
gab_alephit <gabriel@alephit.com.au>
2020-09-07 08:55:48 +0800

    fixes code execution

--- src/BuildCommand.php
+++ src/BuildCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AlephIt\BitbucketWebhook;
 
 use Illuminate\Console\Command;
@@ -14,32 +16,56 @@ class BuildCommand extends Command
     public function handle()
     {
         $branch = $this->argument('branch');
+        echo "<pre>Deploying {$branch} branch</pre><br>";
 
-        echo "<pre>Deploying {$branch} branch</pre>";
+        $this->pullChanges($branch);
+        $this->composerInstallation();
+        $this->migrateAndSeedNecessaryData();
+        $this->purgeCache();
 
+        echo "<pre>Deployment complete</pre><br>";
+    }
+
+    private function pullChanges(string $branch): void
+    {
         shell_exec("git pull origin {$branch}");
         echo "<pre>Pulled changes from {$branch}<br></pre>";
+    }
 
-        $output = shell_exec("composer install --no-dev --no-scripts --no-interaction --prefer-dist --optimize-autoloader");
+    private function composerInstallation(): void
+    {
+        $noDev = "--no-dev";
+        $noInteraction = "--no-interaction";
+        $preferDist = "--prefer-dist";
+        $optimizedAutoloader = "--optimize-autoloader";
+        $output = shell_exec("composer install {$noDev}  {$noInteraction} {$preferDist} {$optimizedAutoloader}");
         echo "<pre>$output</pre>";
+    }
 
+    private function migrateAndSeedNecessaryData(): void
+    {
         echo "<pre>Running database migration</pre>";
         Artisan::call("migrate --seed");
         echo "<pre>Database migration complete</pre>";
+    }
 
+    private function purgeCache(): void
+    {
         echo "<pre>Clearing Config, Cache, Config</pre>";
         Artisan::call("view:clear");
-        Artisan::call("cache:clear");
         Artisan::call("config:clear");
-
-        echo "<pre>Linking storage</pre>";
-        Artisan::call("storage:link");
+        Artisan::call("route:clear");
+        Artisan::call("cache:clear");
 
         echo "<pre>Caching content</pre>";
         Artisan::call("view:cache");
         Artisan::call("config:cache");
         Artisan::call("route:cache");
+    }
 
-        echo "<pre>Deployment complete</pre>";
+    private function linkStorage(): void
+    {
+        echo "<pre>Linking storage</pre>";
+        Artisan::call("storage:link");
     }
 }
